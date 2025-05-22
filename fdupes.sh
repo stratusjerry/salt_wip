8615a7eeef0b51453bf0021c7b6dd65276fe3d68
stratusjerry <icandevops@gmail.com>
2025-05-22 11:47:35 -0400

    win docker fix

--- fdupes.sh
+++ fdupes.sh
@@ -1,11 +1,14 @@
 cd dedupe
-docker run -it -v ./output:/app:ro rockylinux:8.9 /bin/bash
+#docker run -it -v ./output:/app:ro rockylinux:8.9 /bin/bash
+# Stupid window fix below
+docker run -it -v .\output\:/app/foo:ro rockylinux:8.9 /bin/bash
 
 dnf install -y epel-release
 dnf install -y fdupes
 
 # Below not work, app dir mount issue?
 fdupes -r /app/
+# 39,411 files
 
 # Deduplicate recursively, delete duplicates, keep first copy (non-interactive)
-fdupes -rdN /app/
+#fdupes -rdN /app/
